28848fe9f87ab66ca88b2e524028098348e0a7c5
rhuan.carvalho <rhuan.carvalho@totvs.com.br>
2025-04-04 08:23:05 -0300

    Funcionalidade de edicao para edicao component

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './editar-pensamento.component.html',
   styleUrl: './editar-pensamento.component.css'
 })
 export class EditarPensamentoComponent {
+
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(id!).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  editarPensamento() {
+    this.service.editar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+
 
 }
